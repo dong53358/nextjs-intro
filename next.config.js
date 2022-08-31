244a3c3d2946a8bd9cf2100aadf15472305cff58
dong53358 <dong53358@naver.com>
2022-08-31 23:52:41 +0900

    patterns, fetch, redirect and rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+const BASE = "api.themoviedb.org/3/";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://${BASE}movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,51 @@
+import { getModuleBuildInfo } from "next/dist/build/webpack/loaders/get-module-build-info";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
 export default function Home() {
+  const API_KEY = "5c5750bf608396f58f7a40244eee6470";
+  const BASE = "api.themoviedb.org/3/";
+  const BASE_IMAGE = "image.tmdb.org/t/p/w500/";
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello </h1>
+    <div className="container">
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://${BASE_IMAGE}/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          place-items: center;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
